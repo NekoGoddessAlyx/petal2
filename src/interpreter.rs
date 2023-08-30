c93a7cc50e0ca8baf33ea88c21bffbd8280e808e
Alyxandria Rose <NekoGoddessAlyx@users.noreply.github.com>
2023-08-30 10:00:21 -0700

    macroroni and cheese (don't use pointers)

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,4 @@
-use crate::prototype::{ConstantIndex, Instruction, Prototype, Register};
+use crate::prototype::{Instruction, Prototype};
 use crate::value::Value;
 
 pub enum InterpretResult {}
@@ -21,14 +21,21 @@ pub fn interpret(function: Prototype) -> Result<Value, InterpretResult> {
     let mut pc: usize = 0;
     stack.resize(function.stack_size as usize + 1, Value::Integer(0));
 
-    // 🦶🔫
-    let stack = &mut stack as *mut Vec<Value>;
-    let peek = |register: Register| unsafe { *(&*stack).get_unchecked(register as usize) };
-    let mov = |register: Register, value: Value| unsafe {
-        *(&mut *stack).get_unchecked_mut(register as usize) = value;
-    };
-    let get_constant =
-        |constant: ConstantIndex| unsafe { *constants.get_unchecked(constant as usize) };
+    macro_rules! peek {
+        ($r: expr) => {
+            *stack.get_unchecked($r as usize)
+        };
+    }
+    macro_rules! mov {
+        ($r: expr, $v: expr) => {
+            *stack.get_unchecked_mut($r as usize) = $v
+        };
+    }
+    macro_rules! constant {
+        ($c: expr) => {
+            *constants.get_unchecked($c as usize)
+        };
+    }
 
     unsafe {
         loop {
@@ -36,33 +43,33 @@ pub fn interpret(function: Prototype) -> Result<Value, InterpretResult> {
             pc += 1;
             match instruction {
                 Instruction::Return { register } => {
-                    return Ok(peek(register));
+                    return Ok(peek!(register));
                 }
                 Instruction::LoadConstant {
                     destination,
                     constant,
-                } => mov(destination, get_constant(constant)),
-                Instruction::Neg { destination, right } => mov(destination, -peek(right)),
+                } => mov!(destination, constant!(constant)),
+                Instruction::Neg { destination, right } => mov!(destination, -peek!(right)),
                 Instruction::Add {
                     destination,
                     left,
                     right,
-                } => mov(destination, peek(left) + peek(right)),
+                } => mov!(destination, peek!(left) + peek!(right)),
                 Instruction::Sub {
                     destination,
                     left,
                     right,
-                } => mov(destination, peek(left) - peek(right)),
+                } => mov!(destination, peek!(left) - peek!(right)),
                 Instruction::Mul {
                     destination,
                     left,
                     right,
-                } => mov(destination, peek(left) * peek(right)),
+                } => mov!(destination, peek!(left) * peek!(right)),
                 Instruction::Div {
                     destination,
                     left,
                     right,
-                } => mov(destination, peek(left) / peek(right)),
+                } => mov!(destination, peek!(left) / peek!(right)),
             };
         }
     }
