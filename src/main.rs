90dab8f7f19c2c730ded58a6cfb7314590babffd
Alyxandria Rose <NekoGoddessAlyx@users.noreply.github.com>
2023-09-15 15:45:10 -0700

    run from command line with clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::read_to_string;
+use std::path::Path;
 
+use clap::{crate_authors, crate_description, crate_version, Arg, Command};
 use gc_arena::rootless_arena;
 
 use petal2::{compile, interpret, CompilerMessage, PString};
@@ -9,7 +11,37 @@ fn callback(message: CompilerMessage<PString>) {
 }
 
 fn main() {
-    let source = read_to_string("scripts/test.pt").expect("Could not read test script");
+    let command = Command::new("petal")
+        .arg_required_else_help(true)
+        .about(crate_description!())
+        .author(crate_authors!())
+        .version(crate_version!())
+        .disable_version_flag(true)
+        .arg(
+            // all for lowercase v
+            Arg::new("version")
+                .help("Print version")
+                .short('v')
+                .long("version")
+                .action(clap::ArgAction::Version),
+        )
+        .arg(
+            Arg::new("file")
+                .help("File to interpret")
+                .num_args(1)
+                .index(1)
+                .required(true),
+        );
+
+    let matches = command.get_matches();
+
+    let path = matches.get_one::<String>("file").expect("File is required");
+    let path = Path::new(&path);
+    if !path.exists() {
+        panic!("Path '{}' does not exist", path.display());
+    }
+
+    let source = read_to_string(path).expect("Could not read file");
 
     rootless_arena(|mc| {
         let compile_result = compile(mc, callback, source);
